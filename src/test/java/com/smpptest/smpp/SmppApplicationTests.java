745efee54e4f6628364c6103740663f468de47df
Gasimzade09 <gasimzade09@gmail.com>
2019-11-29 09:34:04 +0400

    smpp

---
+++ src/test/java/com/smpptest/smpp/SmppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smpptest.smpp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
